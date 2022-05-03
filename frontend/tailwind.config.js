ce94a4b24c81764fb5d09b6bee23faa16624193a
Daniel Vidal <danielsv465@hotmail.com>
2022-05-03 17:17:44 -0300

    Iniciando frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+module.exports = {
+	content: ['./src/**/*.tsx'],
+	theme: {
+		extend: {
+			colors: {
+				brand: {
+					500: '#8257e6'
+				}
+			}
+		},
+	},
+	plugins: [],
+};
